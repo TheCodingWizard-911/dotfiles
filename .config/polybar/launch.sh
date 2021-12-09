0d5325ee2a63ebaa13c1c00f1e167f680cd9ce06
Ganesh Gitte <ganeshs.gitte911@gmail.com>
2021-12-09 21:26:55 +0530

    initial commit

---
+++ .config/polybar/launch.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Terminate already running bar instances
+killall -q polybar
+
+# Wait until the processes have been shut down
+while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+
+# Bars
+
+polybar -rq keyboard 2>&1 | tee -a /tmp/polybar.log & disown
+polybar -rq cpu 2>&1 | tee -a /tmp/polybar.log & disown
+polybar -rq ram 2>&1 | tee -a /tmp/polybar.log & disown
+polybar -rq linux 2>&1 | tee -a /tmp/polybar.log & disown
+polybar -rq power 2>&1 | tee -a /tmp/polybar.log & disown
+polybar -rq bspwm 2>&1 | tee -a /tmp/polybar.log & disown
+polybar -rq window 2>&1 | tee -a /tmp/polybar.log & disown
+polybar -rq date 2>&1 | tee -a /tmp/polybar.log & disown
+polybar -rq time 2>&1 | tee -a /tmp/polybar.log & disown
+# polybar -rq wifi 2>&1 | tee -a /tmp/polybar.log & disown
+polybar -rq battery 2>&1 | tee -a /tmp/polybar.log & disown
+polybar -rq launcher 2>&1 | tee -a /tmp/polybar.log & disown
